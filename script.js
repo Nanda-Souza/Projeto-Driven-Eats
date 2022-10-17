7d707ee5b7fa288e8b306fa18832a515d696ad29
Nanda-Souza <fesouoli@gmail.com>
2022-10-17 01:06:25 -0300

    Versao Final sem Bonus

--- script.js
+++ script.js
@@ -2,6 +2,13 @@ let selecaoPrato = false;
 let selecaoBebida = false;
 let selecaoSobremesa = false;
 let pedidoPronto = false;
+let prato;
+let bebida;
+let sobremesa;
+let preco_prato;
+let preco_bebida;
+let preco_sobremesa;
+
 
 //função que confirma que 3 items foram selecionados
 function pedidosEscolhidos() {
@@ -17,6 +24,31 @@ function pedidosEscolhidos() {
     }
   }
 
+  //função que soma e manda mensagem 
+  function fecharPedido() {
+    if (pedidoPronto === true) {
+      let mensagem;
+      let total;
+      total = preco_prato + preco_bebida + preco_sobremesa;      
+      mensagem =
+        "Olá, gostaria de fazer o pedido:\n" +
+        " - Prato: " +
+        prato +
+        "\n" +
+        " - Bebida: " +
+        bebida +
+        "\n" +
+        " - Sobremesa: " +
+        sobremesa +
+        "\n" +
+        "Total: R$ " +
+        String(total.toFixed(2).replace(".", ","));
+      mensagem = window.encodeURIComponent(mensagem);
+      window.open("https://wa.me/+5551993526094?text=" + mensagem);
+    }
+  }
+
+  //inicio funções para cada prato 
 function escolherHamburger() {
     // adicionar borda verde na seleção do hamburger
     document.getElementById("hamburger").style.borderColor = "#32B72F"  
@@ -27,6 +59,7 @@ function escolherHamburger() {
     document.getElementById("iconeCachorro").style.opacity = "0"     
     selecaoPrato = true;
     prato = "Hamburger"
+    preco_prato = 34.90;
     pedidosEscolhidos();  
 }
 
@@ -40,6 +73,7 @@ function escolherPizza() {
     document.getElementById("iconeCachorro").style.opacity = "0"
     selecaoPrato = true;
     prato = "Pizza de Calabresa"
+    preco_prato = 45.90;
     pedidosEscolhidos();
 }
 
@@ -53,6 +87,7 @@ function escolherCachorro() {
     document.getElementById("iconeHamburguer").style.opacity = "0"
     selecaoPrato = true;
     prato = "Cachorro Quente"
+    preco_prato = 20.90;
     pedidosEscolhidos();
 }
 
@@ -66,6 +101,7 @@ function escolherCoquinha() {
     document.getElementById("iconeGuarana").style.opacity = "0"
     selecaoBebida = true;
     bebida = "Coquinha gelada"
+    preco_bebida = 6.90;
     pedidosEscolhidos();
 }
 
@@ -78,11 +114,12 @@ function escolherGuarana() {
     document.getElementById("cha").style.borderColor = "white"
     document.getElementById("iconeCha").style.opacity = "0"
     selecaoBebida = true;
-    bebida = "Guaraná gelado"  
+    bebida = "Guaraná gelado" 
+    preco_bebida = 5.90; 
     pedidosEscolhidos(); 
 }
 function escolherCha() {
-    // adicionar borda verde na seleção do guarana
+    // adicionar borda verde na seleção do cha
     document.getElementById("cha").style.borderColor = "#32B72F"
     document.getElementById("iconeCha").style.opacity = "1"
     document.getElementById("coquinha").style.borderColor = "white" 
@@ -91,6 +128,7 @@ function escolherCha() {
     document.getElementById("iconeGuarana").style.opacity = "0"
     selecaoBebida = true;
     bebida = "Chá gelado"   
+    preco_bebida = 8.90;
     pedidosEscolhidos();
 }
 
@@ -104,6 +142,7 @@ function escolherTortaLimao() {
     document.getElementById("iconeTortaChoco").style.opacity = "0"
     selecaoSobremesa = true;
     sobremesa = "Torta de Limão"
+    preco_sobremesa = 9.90;
     pedidosEscolhidos();
 }
 
@@ -117,6 +156,7 @@ function escolherTortaVelvet() {
     document.getElementById("iconeTortaChoco").style.opacity = "0"
     selecaoSobremesa = true;
     sobremesa = "Torta Red Velvet"
+    preco_sobremesa = 12.90;
     pedidosEscolhidos();
 }
 
@@ -130,6 +170,7 @@ function escolherTortaChocolate() {
     document.getElementById("iconeTortaVelvet").style.opacity = "0"
     selecaoSobremesa = true;
     sobremesa = "Torta de Chocolate"
+    preco_sobremesa = 11.90;
     pedidosEscolhidos();
 }
 
